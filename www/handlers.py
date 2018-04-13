6cfeef734e5fe599032b3667123a8f53e2a3fd0b
Ushio <jylzs369@163.com>
2018-04-13 16:33:58 +0800

    修改博客列表页

--- www/handlers.py
+++ www/handlers.py
@@ -73,16 +73,10 @@ async def cookie2user(cookie_str):
         return None
 
 @get('/')
-def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
-    ]
+async def index(*, page='1'):
     return {
         '__template__': 'blogs.html',
-        'blogs': blogs
+        'page_index': get_page_index(page)
     }
 
 @get('/blog/{id}')
